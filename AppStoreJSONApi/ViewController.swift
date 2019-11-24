5713af754e7dc0da991833b493faf92e6c312f47
kiras <kiras0518@gmail.com>
2019-11-24 15:24:38 +0800

    Initial Commit

---
+++ AppStoreJSONApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApi
+//
+//  Created by kiras on 2019/11/24.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
