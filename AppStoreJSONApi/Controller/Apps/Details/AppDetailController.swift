d129b1ab85c003f929e5589adf3c7a2326b11aae
kiras <kiras0518@gmail.com>
2019-12-19 23:18:26 +0800

    JSON Custom CodingKeys

--- AppStoreJSONApi/Controller/Apps/Details/AppDetailController.swift
+++ AppStoreJSONApi/Controller/Apps/Details/AppDetailController.swift
@@ -38,6 +38,9 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
                 }
                 
                 self.reviews = reviews
+                
+                reviews?.feed.entry.forEach({print($0.rating.label)})
+                
                 DispatchQueue.main.async {
                     self.collectionView.reloadData()
                 }
@@ -106,6 +109,10 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
             
         }
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 0, bottom: 16, right: 0)
     }
     
 }
--- AppStoreJSONApi/Controller/Apps/Details/ReviewsController.swift
+++ AppStoreJSONApi/Controller/Apps/Details/ReviewsController.swift
@@ -37,6 +37,17 @@ class ReviewsController: HorizontalSnappingController, UICollectionViewDelegateF
         cell.titleLabel.text = entry?.title.label
         cell.authorLabel.text = entry?.author.name.label
         cell.bodyLabel.text = entry?.content.label
+        
+        // 判斷評價數字顯示星星數量
+        for(index, view) in cell.starsStackView.arrangedSubviews.enumerated() {
+            
+            if let ratingInt = Int(entry!.rating.label) {
+                
+                view.alpha = index > ratingInt ? 0 : 1
+            }
+            
+        }
+        
         return cell
     }
     
